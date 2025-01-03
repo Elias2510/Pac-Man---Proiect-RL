eee9875db917ee0ff1552a97f3eed5710291d600
AlexGhena <alexandru.ghena-ionescu@s.unibuc.ro>
2025-01-03 17:46:37 +0200

    Sters fisiere adaugate gresit

--- test2.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-
-try:
-    env = gym.make("ALE/MsPacman-v5", render_mode="rgb_array")
-    print("MsPacman environment initialized successfully!")
-    env.close()
-except Exception as e:
-    print(f"Error: {e}")
-
